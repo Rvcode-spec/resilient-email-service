0f0e0a562d15a8aaad278832daed782f331a1050
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-07-10 13:48:26 +0530

    Update

--- resilient-email-service/index.js
+++ resilient-email-service/index.js
@@ -10,22 +10,22 @@ server.get('/',(req,resp)=>{
     resp.send("Welcome to Email-Service");
 })
 
-server.post("/send-email", async (req, res) => {
+server.post("/send-email", async (req,resp) => {
   const result = await EmailService.send(req.body);
   queue.addJob(req.body);
-  res.json(result);
+  resp.json(result);
 });
 
-server.get("/:id", (req, res) => {
+server.get("/:id", (req,resp) => {
   const { id } = req.params;
   const status = EmailService.getStatus(id);
 
   if (status === "unknown") {
-    return res.status(404).json({ id, status: "not_found" });
+    return resp.status(404).json({ id, status: "not_found" });
   }
 
-  res.json({ id, status });
+  resp.json({ id, status });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`));
